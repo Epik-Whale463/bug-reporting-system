ae448b8ceb11a368057f666662148826ba4cc117
P V Charan <pvrcharan2022@gmail.com>
2025-09-20 17:15:11 +0530

    Add webpack aliases for lib/components to support absolute imports on Vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.resolve = config.resolve || {}
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      lib: path.resolve(__dirname, 'lib'),
+      components: path.resolve(__dirname, 'components'),
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
